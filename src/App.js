cd3fe042b35cfb16c52ee6f487e0e3b6e9e7b9bd
NguyenVanPhong2410 <phongham2410@gmail.com>
2023-05-23 01:04:26 +0700

    Cấu hình Router và xây dựng cơ chế tải Layout 1:04 2352023

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "~/routes";
+//import này đã được rút gọn
+import { DefaultLayout } from "./components/Layout";
 
 function App() {
     return (
-        <>
-            <h1>ReactJs App</h1>
-        </>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {/* Dùng map xử lí route */}
+                    {
+                        publicRoutes.map((route, index) => {
+                            // //Nếu layout === null thì không render, ngc lại thì render Defaultlayout
+                            // const Layout = route.layout === null ? Fragment : DefaultLayout;
+
+                            let Layout = DefaultLayout;
+
+                            if (route.layout) {
+                                Layout = route.layout
+                            } else if (route.layout === null) {
+                                Layout = Fragment
+                            }
+
+                            const Page = route.component
+                            return <Route
+                                key={index}
+                                path={route.path}
+                                element={<Layout><Page /></Layout>}
+                            />
+                        })
+                    }
+                </Routes>
+            </div>
+        </Router>
+
     );
 }
 
---
+++ src/components/Layout/components/Header/index.js
@@ -0,0 +1,5 @@
+function Header() {
+    return <h2>Header1</h2>
+}
+
+export default Header;
